be595d580cffe24cb4b140e1ec76a2e449685863
VlaRu <dn210690nvs@gmail.com>
2024-07-14 11:43:58 +0200

    refactor: rewrite class Cards in tu function and add detail card in to

--- src/components/Card/Cards.tsx
+++ src/components/Card/Cards.tsx
@@ -1,56 +1,50 @@
-import { Component } from 'react';
+import { useState } from 'react';
+import { pokemonType } from '../../interfaces/interface';
+import { DetailCard } from '../Detail/Detail';
 import './Cards.css';
 
-export type pokemonType = {
-  name: string;
-  id: string;
-  flavorText: string;
-  images: {
-    small?: string;
-    large?: string;
+export default function Cards({ pokemonData }: { pokemonData: pokemonType[] }) {
+  const [selectedPokemon, setSelectedPokemon] = useState<pokemonType | null>(
+    null
+  );
+
+  const handleCardClick = (pokemon: pokemonType) => {
+    setSelectedPokemon(pokemon);
   };
-};
 
-export interface CreateCardsProps {
-  data: pokemonType[];
-}
-
-export default class Cards extends Component<CreateCardsProps> {
-  render() {
-    const { data } = this.props;
-
-    return (
-      <>
-        {data && data.length > 0 ? (
-          <div className="container-cards">
-            {data.map((pokemon: pokemonType) => (
-              <div
-                key={pokemon.id}
-                className="container-card"
-              >
-                <h2>Name: {pokemon.name}</h2>
-                <div className="container-img_card">
-                  <img
-                    src={pokemon.images.small}
-                    alt="pokemon-img"
-                  />
-                </div>
-                {pokemon.flavorText ? (
-                  <h3 style={{ fontSize: '18px' }}>
-                    Description: {pokemon.flavorText}
-                  </h3>
-                ) : (
-                  <h3>Description is absent</h3>
-                )}
+  return (
+    <>
+      {pokemonData && pokemonData.length > 0 ? (
+        <div className="container-cards">
+          {pokemonData.map((pokemon: pokemonType) => (
+            <div
+              key={pokemon.id}
+              className="container-card"
+              onClick={() => handleCardClick(pokemon)}
+            >
+              <h2>Name: {pokemon.name}</h2>
+              <div className="container-img_card">
+                <img
+                  src={pokemon.images.small}
+                  alt="pokemon-img"
+                />
               </div>
-            ))}
-          </div>
-        ) : data.length === 0 ? (
-          <p className="error-info">Not found...</p>
-        ) : (
-          <p className="error-info">Loading data...</p>
-        )}
-      </>
-    );
-  }
+              {pokemon.flavorText ? (
+                <h3 style={{ fontSize: '18px' }}>
+                  Description: {pokemon.flavorText}
+                </h3>
+              ) : (
+                <h3>Description is absent</h3>
+              )}
+            </div>
+          ))}
+          {selectedPokemon && <DetailCard data={selectedPokemon} />}
+        </div>
+      ) : pokemonData.length === 0 ? (
+        <p className="error-info">Not found...</p>
+      ) : (
+        <p className="error-info">Loading data...</p>
+      )}
+    </>
+  );
 }
